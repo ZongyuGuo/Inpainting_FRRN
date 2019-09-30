01eb35b0a59cc5933a16f6ead4aaa74e37903d19
ZongyuGuo <guozy@mail.ustc.edu.cn>
2019-09-30 12:26:51 +0800

    update

--- src/networks.py
+++ src/networks.py
@@ -41,7 +41,7 @@ class FRRNet(BaseNetwork):
         self.block_num = block_num
         self.dilation_num = block_num // 2
         blocks = []
-        for idx in range(self.block_num):
+        for _ in range(self.block_num):
             blocks.append(FRRBlock())
         self.blocks = nn.ModuleList(blocks)
 
@@ -50,62 +50,62 @@ class FRRNet(BaseNetwork):
 
     def forward(self, x, mask):
         mid_x = []
-        mid_mask = []
+        mid_m = []
 
-        mask_update = mask
-        for dilation_idx in range(self.dilation_num):
-            x, _ = self.blocks[dilation_idx * 2](x, mask_update, mask)
-            x, mask_update = self.blocks[dilation_idx * 2 + 1](x, mask_update, mask)
+        mask_new = mask
+        for index in range(self.dilation_num):
+            x, _ = self.blocks[index * 2](x, mask_new, mask)
+            x, mask_new = self.blocks[index * 2 + 1](x, mask_new, mask)
             mid_x.append(x)
-            mid_mask.append(mask_update)
+            mid_m.append(mask_new)
 
-        return x, mid_x, mid_mask
+        return x, mid_x, mid_m
 
 
 class FRRBlock(nn.Module):
     def __init__(self):
         super(FRRBlock, self).__init__()
-        self.full_conv1 = PartialConvLayer(in_channels=3, out_channels=32, kernel_size=5, stride=1, padding=2, use_norm=False)
-        self.full_conv2 = PartialConvLayer(in_channels=32, out_channels=32, kernel_size=5, stride=1, padding=2, use_norm=False)
-        self.full_conv3 = PartialConvLayer(in_channels=32, out_channels=3, kernel_size=5, stride=1, padding=2, use_norm=False)
+        self.full_conv1 = PConvLayer(in_channels=3, out_channels=32, kernel_size=5, stride=1, padding=2, use_norm=False)
+        self.full_conv2 = PConvLayer(in_channels=32, out_channels=32, kernel_size=5, stride=1, padding=2, use_norm=False)
+        self.full_conv3 = PConvLayer(in_channels=32, out_channels=3, kernel_size=5, stride=1, padding=2, use_norm=False)
         self.upsample = nn.Upsample(scale_factor=2, mode='nearest')
-        self.branch_conv1 = PartialConvLayer(in_channels=3, out_channels=64, kernel_size=3, stride=2, padding=1, use_norm=False)
-        self.branch_conv2 = PartialConvLayer(in_channels=64, out_channels=96, kernel_size=3, stride=2, padding=1)
-        self.branch_conv3 = PartialConvLayer(in_channels=96, out_channels=128, kernel_size=3, stride=2, padding=1)
-        self.branch_conv4 = PartialConvLayer(in_channels=128, out_channels=96, kernel_size=3, stride=1, padding=1, act='LeakyReLU')
-        self.branch_conv5 = PartialConvLayer(in_channels=96, out_channels=64, kernel_size=3, stride=1, padding=1, act='LeakyReLU')
-        self.branch_conv6 = PartialConvLayer(in_channels=64, out_channels=3, kernel_size=3, stride=1, padding=1, act='Tanh')
+        self.branch_conv1 = PConvLayer(in_channels=3, out_channels=64, kernel_size=3, stride=2, padding=1, use_norm=False)
+        self.branch_conv2 = PConvLayer(in_channels=64, out_channels=96, kernel_size=3, stride=2, padding=1)
+        self.branch_conv3 = PConvLayer(in_channels=96, out_channels=128, kernel_size=3, stride=2, padding=1)
+        self.branch_conv4 = PConvLayer(in_channels=128, out_channels=96, kernel_size=3, stride=1, padding=1, act='LeakyReLU')
+        self.branch_conv5 = PConvLayer(in_channels=96, out_channels=64, kernel_size=3, stride=1, padding=1, act='LeakyReLU')
+        self.branch_conv6 = PConvLayer(in_channels=64, out_channels=3, kernel_size=3, stride=1, padding=1, act='Tanh')
 
     def forward(self, input, mask, mask_ori):
         x = input
-        out_full, mask_full = self.full_conv1(x, mask)
-        out_full, mask_full = self.full_conv2(out_full, mask_full)
-        out_full, mask_full = self.full_conv3(out_full, mask_full)
+        out_f, mask_f = self.full_conv1(x, mask)
+        out_f, mask_f = self.full_conv2(out_f, mask_f)
+        out_f, mask_f = self.full_conv3(out_f, mask_f)
 
-        out_branch, mask_branch = self.branch_conv1(x, mask)
-        out_branch, mask_branch = self.branch_conv2(out_branch, mask_branch)
-        out_branch, mask_branch = self.branch_conv3(out_branch, mask_branch)
+        out_b, mask_b = self.branch_conv1(x, mask)
+        out_b, mask_b = self.branch_conv2(out_b, mask_b)
+        out_b, mask_b = self.branch_conv3(out_b, mask_b)
 
-        out_branch = self.upsample(out_branch)
-        mask_branch = self.upsample(mask_branch)
-        out_branch, mask_branch = self.branch_conv4(out_branch, mask_branch)
-        out_branch = self.upsample(out_branch)
-        mask_branch = self.upsample(mask_branch)
-        out_branch, mask_branch = self.branch_conv5(out_branch, mask_branch)
-        out_branch = self.upsample(out_branch)
-        mask_branch = self.upsample(mask_branch)
-        out_branch, mask_branch = self.branch_conv6(out_branch, mask_branch)
+        out_b = self.upsample(out_b)
+        mask_b = self.upsample(mask_b)
+        out_b, mask_b = self.branch_conv4(out_b, mask_b)
+        out_b = self.upsample(out_b)
+        mask_b = self.upsample(mask_b)
+        out_b, mask_b = self.branch_conv5(out_b, mask_b)
+        out_b = self.upsample(out_b)
+        mask_b = self.upsample(mask_b)
+        out_b, mask_b = self.branch_conv6(out_b, mask_b)
 
-        mask_update = mask_full * mask_branch
-        out = (out_full * mask_update + out_branch * mask_update) / 2 * (1 - mask_ori) + input
-        #out = (out_full * mask_update + out_branch * mask_update) / 2 * (1 - mask_ori) + input * mask_ori
-        return out, mask_update
+        mask_new = mask_f * mask_b
+        out = (out_f * mask_new + out_b * mask_new) / 2 * (1 - mask_ori) + input
+        #out = (out_f * mask_new + out_b * mask_new) / 2 * (1 - mask_ori) + input * mask_ori
+        return out, mask_new
 
 
 
-class PartialConvLayer(nn.Module):
+class PConvLayer(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding, act='ReLU', use_norm=True):
-        super(PartialConvLayer, self).__init__()
+        super(PConvLayer, self).__init__()
         self.conv = PartialConv2d(in_channels=in_channels, out_channels=out_channels, 
                         kernel_size=kernel_size, stride=stride, padding=padding, return_mask=True)
         self.norm = nn.InstanceNorm2d(out_channels, track_running_stats=False)
